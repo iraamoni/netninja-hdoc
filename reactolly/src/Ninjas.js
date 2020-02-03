b329667aa20d2ba6039e9355aa26aaf887a53e1b
Foysal Ahamed <cfaion341@gmail.com>
2020-02-03 12:34:44 +0100

    stateless components

--- reactolly/src/Ninjas.js
+++ reactolly/src/Ninjas.js
@@ -1,24 +1,21 @@
-import React, { Component} from 'react';
+import React from 'react';
 //import App from './App';
 
-class Ninjas extends Component {
-    render() {
-        const { ninjas } = this.props;
-        const ninjaList = ninjas.map(ninja => {
-            return (
-                <div className = 'ninja' key = {ninja.id} >
-                    <div> Name: { ninja.name }</div>
-                    <div> Age: { ninja.age } </div>
-                    <div> Hair Colour: { ninja.haircolor } </div>
-                </div>
-            )
-        })
+const Ninjas = ({ninjas}) => {
+    const ninjaList = ninjas.map(ninja => {
         return (
-            <div className = 'ninja-list'>
-                { ninjaList }
+            <div className = 'ninja' key = {ninja.id} >
+                <div> Name: { ninja.name }</div>
+                <div> Age: { ninja.age } </div>
+                <div> Hair Colour: { ninja.haircolor } </div>
             </div>
         )
-    }
+    })
+    return (
+        <div className = 'ninja-list'>
+            { ninjaList }
+        </div>
+    )
 }
 
 export default Ninjas;
