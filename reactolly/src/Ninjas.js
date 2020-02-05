16af95e4c732314b9013ec35c1e022187b49420f
Foysal Ahamed <cfaion341@gmail.com>
2020-02-05 10:29:08 +0100

    ternary if else

--- reactolly/src/Ninjas.js
+++ reactolly/src/Ninjas.js
@@ -1,15 +1,34 @@
 import React from 'react';
 //import App from './App';
 
+// const Ninjas = ({ninjas}) => {
+//     const ninjaList = ninjas.map(ninja => {
+//         if (ninja.age > 24) {
+//             return (
+//                 <div className = 'ninja' key = {ninja.id} >
+//                     <div> Name: { ninja.name }</div>
+//                     <div> Age: { ninja.age } </div>
+//                     <div> Hair Colour: { ninja.haircolor } </div>
+//                 </div>
+//             )
+//         } else {
+//             return null
+//         }
+//     })
+//     return (
+//         <div className = 'ninja-list'>
+//             { ninjaList }
+//         </div>
+//     )
+// }
+
+// export default Ninjas;
+
+// ternary operator example
+
 const Ninjas = ({ninjas}) => {
     const ninjaList = ninjas.map(ninja => {
-        return (
-            <div className = 'ninja' key = {ninja.id} >
-                <div> Name: { ninja.name }</div>
-                <div> Age: { ninja.age } </div>
-                <div> Hair Colour: { ninja.haircolor } </div>
-            </div>
-        )
+        ninja.age > 20 ? () : null;
     })
     return (
         <div className = 'ninja-list'>
