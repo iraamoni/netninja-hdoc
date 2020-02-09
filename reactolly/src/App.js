4ede72ddd8e597d01b079f981495f705e1025fbf
Suborna Jahan <suborna@Subornas-MBP.homenet.telecomitalia.it>
2020-02-09 23:51:26 +0100

    forms revisited

--- reactolly/src/App.js
+++ reactolly/src/App.js
@@ -10,13 +10,22 @@ class App extends Component {
             {name: 'Mikey', age: 23, haircolor: 'blond', id: 3},
         ]
     }
+
+    addNinja = (ninja) => {
+        ninja.id = Math.random();
+        let ninjas = [...this.state.ninjas, ninja]
+        this.setState ({
+            ninjas: ninjas
+        })
+    }
+
     render() {
         return (
             <div className = "App" >
                 <h1> My first React app </h1> 
                 <p> Welcome: ) </p> 
                 <Ninjas ninjas= { this.state.ninjas }/>
-                <AddNinja />
+                <AddNinja addNinja = {this.addNinja}/>
             </div>
         );
     }
