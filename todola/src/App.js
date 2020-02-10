0b8dfd193101c44cb4de844987675997f69333ce
Suborna Jahan <suboorna@gmail.com>
2020-02-10 22:51:08 +0100

    can delete todos by clicking on the texts

--- todola/src/App.js
+++ todola/src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react'
+import Todos from './Todos'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+    state = {
+        todos: [
+            {id: 1, content: 'milk'},
+            {id: 2, content: 'Potato'},
+            {id: 3, content: 'Coconut'},
+            {id: 4, content: 'Chocolate'}
+        ]
+    }
+
+    deleteTodo = (id) => {
+        
+        const todos = this.state.todos.filter(todo => {
+            return todo.id != id;
+        });
+
+        this.setState({
+            todos
+        })
+    }
+
+    render() {
+        return (
+            <div className = 'todo-app container'>
+                <h1 className = 'center blue-text'>Todo's</h1> 
+                <Todos todos = { this.state.todos } deleteTodo = {this.deleteTodo} />
+            </div>
+        );
+    }
 }
 
-export default App;
+export default App
