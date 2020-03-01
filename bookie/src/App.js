11a3e61c500ae6adcc063fd6075293789e1b090e
Suborna Jahan <suboorna@gmail.com>
2020-03-01 22:28:18 +0100

    create multiple context

--- bookie/src/App.js
+++ bookie/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
+
 import Navbar from './components/Navbar';
 import BookList from './components/BookList';
-import ThemeContextProvider from './contexts/ThemeContext';
 import ThemeToggle from './components/ThemeToggle';
+
+import ThemeContextProvider from './contexts/ThemeContext';
+import AuthContextProvider from './contexts/AuthContext';
 
 
 function App() {
     return (
         <div className="App">
             <ThemeContextProvider>
-                <Navbar />
-                <BookList />
-                <ThemeToggle />
+                <AuthContextProvider>
+                    <Navbar />
+                    <BookList />
+                    <ThemeToggle />
+                </AuthContextProvider>
             </ThemeContextProvider>
         </div>
     );
